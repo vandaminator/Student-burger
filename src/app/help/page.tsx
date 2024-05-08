e655d665fb6fa15966b6466e8a89f4dc1c9c0d88
vandaminator <ntokoanephomolo76@gmail.com>
2024-05-08 19:24:37 +0200

    help page update phone number

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -5,7 +5,7 @@ function Helppage() {
     <main className="space-y-4">
       <h1 className="text-3xl text-primary underline">Help page</h1>
       <div className="">
-        If there are any problems with the website please contact <a href="tel:+26658539540">+266 5853 9540</a>
+        If there are any problems with the website please contact <a href="tel:+26658967429">+266 5896 7429</a>
       </div>
     </main>
   )
